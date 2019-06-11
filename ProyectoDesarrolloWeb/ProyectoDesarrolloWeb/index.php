a5f9e5bef0d2683826f389ac04695b47934ce7b8
cermon21 <cesarcarmona97@gmail.com>
2019-06-10 22:43:41 -0500

    Update index.php

--- ProyectoDesarrolloWeb/ProyectoDesarrolloWeb/index.php
+++ ProyectoDesarrolloWeb/ProyectoDesarrolloWeb/index.php
@@ -179,8 +179,52 @@
                 } else {
                 }
             }
-        # Termina Comprobar acceso
+        }
     ?> 
+
+    <?php include('conexionDB/conexion.php'); ?>
+
+    <?php
+        /**
+        * Sentencias SQL que permiten obtener el usuario
+        * a través de la base de datos e implementando
+        * los datos obtenidos en el formulario
+        * por medio de un método POST
+        **/
+        $sql="SELECT * FROM PERSONA WHERE Usuario='".$_POST['correo']."' AND Contrasena='".$_POST['contrasena']."'";
+        /**
+        * Implementación de una función MySQLi para
+        * generar consultas
+        **/
+        $result = mysqli_query($conexion,$sql);
+        $datos = mysqli_fetch_array($result);
+
+        /**
+        * Método que genera una cookie con el identicador
+        * del usuario que inicia sesión
+        **/
+        setcookie('id_persona',$datos['Id_persona']);
+        if (mysqli_num_rows($result) == 0) {
+            /**
+            * Alerta generada en JavaScript en caso que
+            * el usuario digíte erroneamente sus
+            * credenciales de acceso al portal web
+            **/
+            echo '<script language="javascript">';
+            echo 'alert("Usuario o contraseña incorrecta")';
+            echo '</script>';
+        } else {
+            /**
+            * Alerta generada en JavaScript en caso que
+            * el usuario digíte correctamente sus
+            * credenciales de acceso al portal web
+            **/
+            echo '<script language="javascript">';
+            echo 'alert("Bienvenido")';
+            echo '</script>';
+        }
+
+    ?>
 
     <?php
         # Empieza Buscar Bici
